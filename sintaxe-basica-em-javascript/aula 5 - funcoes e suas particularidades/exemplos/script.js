8d91926fb477525b0efc10231d22f841d2c391f6
Nathallye <nathallyet@gmail.com>
2021-12-30 21:47:18 -0300

    Slide e exemplos aula 5 sobre funcoes.

---
+++ sintaxe-basica-em-javascript/aula 5 - funcoes e suas particularidades/exemplos/script.js
@@ -0,0 +1,14 @@
+// Funções
+function nomeDaFuncao() {
+  console.log("Exibir alguma mensagem...") // instrução
+}
+
+nomeDaFuncao();
+
+
+// Funções com parâmetros
+function funcao(parametro) {
+  console.log(parametro) // instrução
+}
+
+funcao("Valor do parametro");
